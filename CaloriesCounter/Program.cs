c2e40247b3ce67d7388f2807967128449f354bd7
Malin-Naydenov <malinski_k@Abv.bg>
2017-10-19 20:19:02 +0300

    New project

---
+++ CaloriesCounter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace CaloriesCounter
+{
+    class Program
+    {
+        static void Main()
+        {
+            int countIngredients = int.Parse(Console.ReadLine());
+            double totalCalories = 0;
+            for (int i = 0; i < countIngredients; i++)
+            {
+                string ingredients = Console.ReadLine().ToLower();
+
+                switch (ingredients)
+                {
+                    case "cheese": totalCalories += 500;break;
+                    case "tomato sauce": totalCalories += 150;break;
+                    case "salami": totalCalories += 600;break;
+                    case "pepper": totalCalories += 50; break;
+                    default: totalCalories += 0;break;
+                }
+            }
+            
+            Console.WriteLine($"Total calories: {totalCalories}");
+        }
+    }
+}
