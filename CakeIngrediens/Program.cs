7b94562ba52331089d2eb67ad69a1c4420d2a8c1
Malin-Naydenov <malinski_k@Abv.bg>
2017-10-19 20:13:58 +0300

    First code

---
+++ CakeIngrediens/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace CakeIngrediens
+{
+    class Program
+    {
+        static void Main()
+        {
+            int countIngredient = 0;
+            for (int i = 1; i <= 20; i++)
+            {
+                string ingredient = Console.ReadLine();
+                if (ingredient == "Bake!")
+                {
+                    break;
+                }
+                Console.WriteLine($"Adding ingredient {ingredient}.");
+                countIngredient++;
+            }
+            Console.WriteLine($"Preparing cake with {countIngredient} ingredients.");
+        }
+    }
+}
